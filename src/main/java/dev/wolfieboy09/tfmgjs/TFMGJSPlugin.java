9023390ac5d662f9c320ab2bc273a5dc6f2da166
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-12-01 13:45:30 -0500

    Distillation recipe

--- src/main/java/dev/wolfieboy09/tfmgjs/TFMGJSPlugin.java
+++ src/main/java/dev/wolfieboy09/tfmgjs/TFMGJSPlugin.java
@@ -5,12 +5,14 @@ import dev.latvian.mods.kubejs.KubeJSPlugin;
 import dev.latvian.mods.kubejs.recipe.schema.RegisterRecipeSchemasEvent;
 import dev.wolfieboy09.tfmgjs.recipes.schema.CastingSchema;
 import dev.wolfieboy09.tfmgjs.recipes.schema.CokingSchema;
+import dev.wolfieboy09.tfmgjs.recipes.schema.DistillationSchema;
 
 public class TFMGJSPlugin extends KubeJSPlugin {
     @Override
     public void registerRecipeSchemas(RegisterRecipeSchemasEvent event) {
         event.namespace(CreateTFMG.MOD_ID)
                 .register("casting", CastingSchema.SCHEMA)
-                .register("coking", CokingSchema.SCHEMA);
+                .register("coking", CokingSchema.SCHEMA)
+                .register("distillation", DistillationSchema.SCHEMA);
     }
 }
--- src/main/java/dev/wolfieboy09/tfmgjs/recipes/schema/DistillationSchema.java
+++ src/main/java/dev/wolfieboy09/tfmgjs/recipes/schema/DistillationSchema.java
@@ -1,8 +1,29 @@
 package dev.wolfieboy09.tfmgjs.recipes.schema;
 
+import dev.latvian.mods.kubejs.fluid.InputFluid;
+import dev.latvian.mods.kubejs.fluid.OutputFluid;
+import dev.latvian.mods.kubejs.recipe.RecipeExceptionJS;
 import dev.latvian.mods.kubejs.recipe.RecipeKey;
+import dev.latvian.mods.kubejs.recipe.component.FluidComponents;
+import dev.latvian.mods.kubejs.recipe.schema.RecipeConstructor;
+import dev.latvian.mods.kubejs.recipe.schema.RecipeSchema;
 
 public interface DistillationSchema {
+    RecipeKey<InputFluid[]> INGREDIENT = FluidComponents.INPUT_ARRAY.key("ingredients");
+    RecipeKey<OutputFluid[]> RESULTS = FluidComponents.OUTPUT_ARRAY.key("results");
 
+    RecipeConstructor.Factory FACTORY = (recipe, schemaType, keys, from) -> {
+        if (from.getValue(recipe, INGREDIENT).length != 1) {
+            throw new RecipeExceptionJS("Recipe must have exactly one fluid input");
+        }
+        recipe.setValue(INGREDIENT, from.getValue(recipe, INGREDIENT));
 
+        if (from.getValue(recipe, RESULTS).length > 6) {
+            throw new RecipeExceptionJS("Recipe can only have up to 6 fluid outputs");
+        }
+        recipe.setValue(RESULTS, from.getValue(recipe, RESULTS));
+    };
+
+    RecipeSchema SCHEMA = new RecipeSchema(INGREDIENT, RESULTS)
+            .constructor(FACTORY, INGREDIENT, RESULTS);
 }
