195c482d6df23b50f635ea22adb108d7dfddfe2c
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-12-01 14:47:13 -0500

    Industrial blasting recipe

--- src/main/java/dev/wolfieboy09/tfmgjs/TFMGJSPlugin.java
+++ src/main/java/dev/wolfieboy09/tfmgjs/TFMGJSPlugin.java
@@ -3,9 +3,7 @@ package dev.wolfieboy09.tfmgjs;
 import com.drmangotea.tfmg.CreateTFMG;
 import dev.latvian.mods.kubejs.KubeJSPlugin;
 import dev.latvian.mods.kubejs.recipe.schema.RegisterRecipeSchemasEvent;
-import dev.wolfieboy09.tfmgjs.recipes.schema.CastingSchema;
-import dev.wolfieboy09.tfmgjs.recipes.schema.CokingSchema;
-import dev.wolfieboy09.tfmgjs.recipes.schema.DistillationSchema;
+import dev.wolfieboy09.tfmgjs.recipes.schema.*;
 
 public class TFMGJSPlugin extends KubeJSPlugin {
     @Override
@@ -13,6 +11,8 @@ public class TFMGJSPlugin extends KubeJSPlugin {
         event.namespace(CreateTFMG.MOD_ID)
                 .register("casting", CastingSchema.SCHEMA)
                 .register("coking", CokingSchema.SCHEMA)
-                .register("distillation", DistillationSchema.SCHEMA);
+                .register("distillation", DistillationSchema.SCHEMA)
+                // .register("gas_blasting", GasBlastingSchema.SCHEMA) //TODO FIX BUG
+                .register("industrial_blasting", IndustrialBlastingSchema.SCHEMA);
     }
 }
