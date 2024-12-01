f247e323501e3fcfea63c6cc7950c6461ee4a232
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-12-01 12:08:59 -0500

    fix plugin not being loaded

--- src/main/java/dev/wolfieboy09/tfmgjs/TFMGJSPlugin.java
+++ src/main/java/dev/wolfieboy09/tfmgjs/TFMGJSPlugin.java
@@ -5,7 +5,7 @@ import dev.latvian.mods.kubejs.KubeJSPlugin;
 import dev.latvian.mods.kubejs.recipe.schema.RegisterRecipeSchemasEvent;
 import dev.wolfieboy09.tfmgjs.recipes.schema.CastingSchema;
 
-public class TFMGPJSPlugin extends KubeJSPlugin {
+public class TFMGJSPlugin extends KubeJSPlugin {
     @Override
     public void registerRecipeSchemas(RegisterRecipeSchemasEvent event) {
         event.namespace(CreateTFMG.MOD_ID)
