c754cf881c940948b42d8ede87c0ecac5613e57e
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-12-07 09:20:40 -0500

    Change how recipe schemas are registered

--- src/main/java/dev/wolfieboy09/tfmgjs/TFMGJSPlugin.java
+++ src/main/java/dev/wolfieboy09/tfmgjs/TFMGJSPlugin.java
@@ -1,20 +1,30 @@
 package dev.wolfieboy09.tfmgjs;
 
-import com.drmangotea.tfmg.CreateTFMG;
+import com.drmangotea.tfmg.registry.TFMGRecipeTypes;
+import com.simibubi.create.content.processing.recipe.ProcessingRecipeSerializer;
 import dev.latvian.mods.kubejs.KubeJSPlugin;
+import dev.latvian.mods.kubejs.recipe.schema.RecipeSchema;
 import dev.latvian.mods.kubejs.recipe.schema.RegisterRecipeSchemasEvent;
-import dev.wolfieboy09.tfmgjs.recipes.schema.*;
+import dev.wolfieboy09.tfmgjs.recipes.*;
+
+import java.util.Map;
 
 public class TFMGJSPlugin extends KubeJSPlugin {
+    private static final Map<TFMGRecipeTypes, RecipeSchema> schemas = Map.of(
+            TFMGRecipeTypes.CASTING, CastingSchema.SCHEMA,
+            TFMGRecipeTypes.COKING, CokingSchema.SCHEMA,
+            TFMGRecipeTypes.DISTILLATION, DistillationSchema.SCHEMA,
+            TFMGRecipeTypes.INDUSTRIAL_BLASTING, IndustrialBlastingSchema.SCHEMA,
+            TFMGRecipeTypes.POLARIZING, PolarizingSchema.SCHEMA
+    );
+
     @Override
     public void registerRecipeSchemas(RegisterRecipeSchemasEvent event) {
-        event.namespace(CreateTFMG.MOD_ID)
-                .register("casting", CastingSchema.SCHEMA)
-                .register("coking", CokingSchema.SCHEMA)
-                .register("distillation", DistillationSchema.SCHEMA)
-                .register("industrial_blasting", IndustrialBlastingSchema.SCHEMA)
-                .register("polarizing", PolarizingSchema.SCHEMA);
-                // .register("gas_blasting", GasBlastingSchema.SCHEMA)
-                // .register("welding", WeldingSchema.SCHEMA);
+        for (TFMGRecipeTypes entry : TFMGRecipeTypes.values()) {
+            if (entry.getSerializer() instanceof ProcessingRecipeSerializer<?>) {
+                RecipeSchema schema = schemas.get(entry);
+                event.register(entry.getId(), schema);
+            }
+        }
     }
 }
