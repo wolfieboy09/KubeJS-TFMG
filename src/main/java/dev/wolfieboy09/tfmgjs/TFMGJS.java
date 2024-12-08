9d5845eb323c964dd18077ea3836fe63e9d156a6
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-12-08 18:00:55 -0500

    move to ModContainer to fix #1

--- src/main/java/dev/wolfieboy09/tfmgjs/TFMGJS.java
+++ src/main/java/dev/wolfieboy09/tfmgjs/TFMGJS.java
@@ -1,8 +1,8 @@
 package dev.wolfieboy09.tfmgjs;
 
 import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.ModContainer;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLModContainer;
 import org.slf4j.Logger;
 
 @Mod(TFMGJS.MODID)
@@ -10,6 +10,6 @@ public class TFMGJS {
     public static final String MODID = "tfmgjs";
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public TFMGJS(FMLModContainer context) {}
+    public TFMGJS(ModContainer context) {}
 
 }
