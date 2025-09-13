575c509266eaeca0dccee9c3d5bdd29ce6a417bb
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-09-13 10:02:49 -0400

    fix confusion from me

--- src/main/java/dev/wolfieboy09/tfmgjs/electrodes/ElectrodeBuilderJS.java
+++ src/main/java/dev/wolfieboy09/tfmgjs/electrodes/ElectrodeBuilderJS.java
@@ -1,6 +1,9 @@
 package dev.wolfieboy09.tfmgjs.electrodes;
 
+import com.drmangotea.tfmg.TFMG;
 import com.drmangotea.tfmg.content.machinery.vat.electrode_holder.electrode.Electrode;
+import com.drmangotea.tfmg.content.machinery.vat.electrode_holder.electrode.ElectrodeBuilder;
+import com.tterrag.registrate.util.entry.ItemEntry;
 import dev.latvian.mods.kubejs.item.ItemBuilder;
 import dev.latvian.mods.kubejs.registry.BuilderBase;
 import dev.latvian.mods.kubejs.registry.RegistryInfo;
@@ -13,15 +16,17 @@ import org.jetbrains.annotations.ApiStatus;
 
 @ApiStatus.Experimental
 public class ElectrodeBuilderJS extends BuilderBase<Electrode> {
-    private final Electrode.Properties electrodeProperties = new Electrode.Properties(this.id);
-    private final Item.Properties spoolProperties = new Item.Properties();
+    private final Electrode.Properties electrodeProperties = new Electrode.Properties(this.id)
+            .operationId(this.id.toString());
+            //TODO .item() requires an ItemEntry<?> from registrate
+    private final Item.Properties electrodeItemProperties = new Item.Properties();
 
     public ElectrodeBuilderJS(ResourceLocation i) {
         super(i);
     }
 
-    @Info(value = "Set's the wire's resistance", params = {
-            @Param("The wire's resistance")
+    @Info(value = "Set's the electrode's resistance", params = {
+            @Param("The electrode's resistance")
     })
     public ElectrodeBuilderJS resistance(int resistance) {
         this.electrodeProperties.resistance(resistance);
@@ -43,7 +48,7 @@ public class ElectrodeBuilderJS extends BuilderBase<Electrode> {
         RegistryInfo.ITEM.addBuilder(new ItemBuilder(this.id) {
             @Override
             public Item createObject() {
-                return new Item(spoolProperties.stacksTo(1));
+                return new Item(electrodeItemProperties);
             }
         });
     }
